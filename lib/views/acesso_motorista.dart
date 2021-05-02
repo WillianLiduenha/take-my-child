3b3eed66a9063411edbfccdbd526a5d3e1de5aa0
Will <willianliduenha@hotmail.com>
2021-05-02 20:59:03 -0300

    Melhorias na tela de acesso (visual)

--- lib/views/acesso_motorista.dart
+++ lib/views/acesso_motorista.dart
@@ -36,20 +36,24 @@ class _Acesso_motorista extends State<acesso_motorista> {
         ),
         centerTitle: false,
       ),
-      body: Container(
-        width: double.infinity,
-        height: double.infinity,
-        color: Colors.yellow,
-        padding: EdgeInsets.only(top: 10),
-        child: Column(
-          children: [
-            Flexible(
-              flex: 1,
+      body: Column(
+        children: [
+          Flexible(
+            flex: 2,
+            child: Container(
+              width: double.infinity,
+              height: double.infinity,
+              //color: Colors.yellow,
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.only(
+                    bottomLeft: Radius.circular(200),
+                  ),
+                  color: Colors.yellow),
               child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                mainAxisAlignment: MainAxisAlignment.center,
                 children: [
                   Container(
-                    height: 130,
+                    height: 200,
                     child: Image.asset(
                       "assets/images/van.png",
                     ),
@@ -58,132 +62,132 @@ class _Acesso_motorista extends State<acesso_motorista> {
                     "Take my child!",
                     style: TextStyle(
                       fontWeight: FontWeight.bold,
-                      fontSize: 30,
+                      fontSize: 45,
                     ),
                   ),
                 ],
               ),
             ),
-            SizedBox(
-              height: 50,
-            ),
-            Flexible(
-              flex: 2,
-              child: Form(
-                key: _formKey,
-                child: Container(
-                  width: double.infinity,
-                  height: double.infinity,
-                  color: Colors.white,
-                  padding: EdgeInsets.symmetric(horizontal: 25, vertical: 40),
-                  child: Column(
-                    children: [
-                      TextFormField(
-                        autofocus: true,
-                        style: TextStyle(color: Colors.black),
-                        cursorColor: Colors.black,
-                        decoration: InputDecoration(
-                          labelText: "Usuário",
-                          labelStyle: TextStyle(color: Colors.black),
-                          focusedBorder: OutlineInputBorder(
-                            borderSide: BorderSide(
-                              color: Colors.black,
-                              width: 1,
-                            ),
-                          ),
-                          enabledBorder: OutlineInputBorder(
-                            borderSide: BorderSide(
-                              color: Colors.black,
-                              width: 1,
-                            ),
-                          ),
-                          fillColor: Color.fromRGBO(240, 230, 140, 0.7),
-                          filled: true,
-                        ),
-                        //onSaved: (value) => _tarefa.texto = value,
-                        validator: (value) =>
-                            value.isEmpty ? "Campo obrigatório" : null,
-                      ),
-                      SizedBox(
-                        height: 5,
-                      ),
-                      TextFormField(
-                        obscureText: true,
-                        style: TextStyle(color: Colors.black),
-                        cursorColor: Colors.black,
-                        decoration: InputDecoration(
-                          labelText: "Senha",
-                          labelStyle: TextStyle(color: Colors.black),
-                          focusedBorder: OutlineInputBorder(
-                            borderSide: BorderSide(
-                              color: Colors.black,
-                              width: 1,
-                            ),
-                          ),
-                          enabledBorder: OutlineInputBorder(
-                            borderSide: BorderSide(
-                              color: Colors.black,
-                              width: 1,
-                            ),
-                          ),
-                          fillColor: Color.fromRGBO(240, 230, 140, 0.7),
-                          filled: true,
-                        ),
-                        //onSaved: (value) => _tarefa.texto = value,
-                        validator: (value) =>
-                            value.isEmpty ? "Campo obrigatório" : null,
-                      ),
-                      SizedBox(
-                        height: 50,
-                      ),
-                      Container(
-                        height: 50,
-                        width: double.infinity,
-                        child: TextButton(
-                          onPressed: () => login(context),
-                          child: Text(
-                            "Entrar",
-                            style: TextStyle(
-                              fontSize: 25,
-                            ),
-                          ),
-                          style: ButtonStyle(
-                            shape: MaterialStateProperty.all<
-                                    RoundedRectangleBorder>(
-                                RoundedRectangleBorder(
-                                    borderRadius: BorderRadius.circular(10))),
-                            foregroundColor:
-                                MaterialStateProperty.all<Color>(Colors.black),
-                            backgroundColor:
-                                MaterialStateProperty.all<Color>(Colors.yellow),
+          ),
+          SizedBox(
+            height: 50,
+          ),
+          Flexible(
+            flex: 2,
+            child: Form(
+              key: _formKey,
+              child: Container(
+                width: double.infinity,
+                height: double.infinity,
+                color: Colors.white,
+                padding: EdgeInsets.symmetric(horizontal: 25, vertical: 40),
+                child: Column(
+                  children: [
+                    TextFormField(
+                      autofocus: true,
+                      style: TextStyle(color: Colors.black),
+                      cursorColor: Colors.black,
+                      decoration: InputDecoration(
+                        labelText: "Usuário",
+                        labelStyle: TextStyle(color: Colors.black),
+                        focusedBorder: OutlineInputBorder(
+                          borderSide: BorderSide(
+                            color: Colors.black,
+                            width: 1,
                           ),
                         ),
+                        enabledBorder: OutlineInputBorder(
+                          borderSide: BorderSide(
+                            color: Colors.black,
+                            width: 1,
+                          ),
+                        ),
+                        fillColor: Color.fromRGBO(240, 230, 140, 0.7),
+                        filled: true,
                       ),
-                      SizedBox(
-                        height: 5,
+                      //onSaved: (value) => _tarefa.texto = value,
+                      validator: (value) =>
+                          value.isEmpty ? "Campo obrigatório" : null,
+                    ),
+                    SizedBox(
+                      height: 5,
+                    ),
+                    TextFormField(
+                      obscureText: true,
+                      style: TextStyle(color: Colors.black),
+                      cursorColor: Colors.black,
+                      decoration: InputDecoration(
+                        labelText: "Senha",
+                        labelStyle: TextStyle(color: Colors.black),
+                        focusedBorder: OutlineInputBorder(
+                          borderSide: BorderSide(
+                            color: Colors.black,
+                            width: 1,
+                          ),
+                        ),
+                        enabledBorder: OutlineInputBorder(
+                          borderSide: BorderSide(
+                            color: Colors.black,
+                            width: 1,
+                          ),
+                        ),
+                        fillColor: Color.fromRGBO(240, 230, 140, 0.7),
+                        filled: true,
                       ),
-                      TextButton(
-                        onPressed: () => Navigator.of(context)
-                            .pushNamed('/cadastromotorista'),
+                      //onSaved: (value) => _tarefa.texto = value,
+                      validator: (value) =>
+                          value.isEmpty ? "Campo obrigatório" : null,
+                    ),
+                    SizedBox(
+                      height: 50,
+                    ),
+                    Container(
+                      height: 50,
+                      width: double.infinity,
+                      child: TextButton(
+                        onPressed: () => login(context),
                         child: Text(
-                          "Não tem uma conta? Cadastre-se",
+                          "Entrar",
                           style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            decoration: TextDecoration.underline,
+                            fontSize: 25,
                           ),
                         ),
                         style: ButtonStyle(
+                          shape:
+                              MaterialStateProperty.all<RoundedRectangleBorder>(
+                                  RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(10))),
                           foregroundColor:
                               MaterialStateProperty.all<Color>(Colors.black),
+                          backgroundColor:
+                              MaterialStateProperty.all<Color>(Colors.yellow),
                         ),
                       ),
-                    ],
-                  ),
+                    ),
+                    SizedBox(
+                      height: 5,
+                    ),
+                    TextButton(
+                      onPressed: () =>
+                          Navigator.of(context).pushNamed('/cadastromotorista'),
+                      child: Text(
+                        "Não tem uma conta? Cadastre-se",
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          decoration: TextDecoration.underline,
+                        ),
+                      ),
+                      style: ButtonStyle(
+                        foregroundColor:
+                            MaterialStateProperty.all<Color>(Colors.black),
+                      ),
+                    ),
+                  ],
                 ),
               ),
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
