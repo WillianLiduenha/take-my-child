656bc99a9e0cfb7b1fecdc571c3d064c2aba2699
Will <willianliduenha@hotmail.com>
2021-05-31 17:35:40 -0300

    editar aluno atualizado... percurso atualizado

--- lib/views/editar_aluno.dart
+++ lib/views/editar_aluno.dart
@@ -15,7 +15,6 @@ class _Editar_Aluno extends State<editar_aluno> {
   ParentsModel _responsaveis = new ParentsModel();
   ResponsavelRepository repository = new ResponsavelRepository();
 
-
   final _formKey = GlobalKey<FormState>();
   create(BuildContext context) {
     if (_formKey.currentState.validate()) {
@@ -107,7 +106,8 @@ class _Editar_Aluno extends State<editar_aluno> {
     print(_responsaveis.route.toString());
     _percurso = _responsaveis.route != 3 && _responsaveis.route == 2
         ? trajeto.volta
-        : trajeto.ida;
+        : _responsaveis.route == 3 ? trajeto.idaVolta : trajeto.ida;
+  
 
     return Scaffold(
       //início da tela
@@ -239,7 +239,6 @@ class _Editar_Aluno extends State<editar_aluno> {
                         ),
                         TextFormField(
                           maxLength: 50,
-                          
                           initialValue: _responsaveis.name_child.toString(),
                           cursorColor: Colors.black,
                           decoration: InputDecoration(
@@ -249,7 +248,8 @@ class _Editar_Aluno extends State<editar_aluno> {
                             enabledBorder: OutlineInputBorder(),
                             counterText: "",
                           ),
-                          onSaved: (value) => _responsaveis.name_child = value.toString(),
+                          onSaved: (value) =>
+                              _responsaveis.name_child = value.toString(),
                           validator: (value) =>
                               value.isEmpty ? "Campo obrigatório" : null,
                         ),
@@ -257,7 +257,7 @@ class _Editar_Aluno extends State<editar_aluno> {
                           height: 5,
                         ),
                         TextFormField(
-                           maxLength: 20,
+                          maxLength: 20,
                           initialValue: _responsaveis.school.toString(),
                           cursorColor: Colors.black,
                           decoration: InputDecoration(
@@ -275,7 +275,7 @@ class _Editar_Aluno extends State<editar_aluno> {
                           height: 5,
                         ),
                         TextFormField(
-                           maxLength: 50,
+                          maxLength: 50,
                           initialValue: _responsaveis.addressSchool.toString(),
                           cursorColor: Colors.black,
                           decoration: InputDecoration(
@@ -285,7 +285,8 @@ class _Editar_Aluno extends State<editar_aluno> {
                             enabledBorder: OutlineInputBorder(),
                             counterText: "",
                           ),
-                          onSaved: (value) => _responsaveis.addressSchool = value,
+                          onSaved: (value) =>
+                              _responsaveis.addressSchool = value,
                           validator: (value) =>
                               value.isEmpty ? "Campo obrigatório" : null,
                         ),
