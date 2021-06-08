de7d7c1e056c8a88126d954c4a651cb0be12f634
Will <willianliduenha@hotmail.com>
2021-06-07 22:44:23 -0300

    status do aluno feito

--- lib/views/alunos_presentes.dart
+++ lib/views/alunos_presentes.dart
@@ -19,6 +19,42 @@ class _AlunosPresentesState extends State<AlunosPresentes> {
   Widget build(BuildContext context) {
     _alunosPresentes = ModalRoute.of(context).settings.arguments;
 
+    Future<void> updateStatus(ShiftModel aluno) async {
+      var resposta = await shiftRepository.updateStatus(aluno);
+      print(resposta);
+      return resposta;
+    }
+
+    // int i = 0;
+    // void ordenar() {
+    //   List<ShiftModel> lista0 = List<ShiftModel>();
+    //   List<ShiftModel> lista1 = List<ShiftModel>();
+    //   List<ShiftModel> lista2 = List<ShiftModel>();
+    //   List<ShiftModel> lista3 = List<ShiftModel>();
+
+    //   while (i < _alunosPresentes.length) {
+    //     print(_alunosPresentes);
+    //     print(lista1);
+    //     print(lista2);
+    //     print(lista3);
+    //     if (_alunosPresentes[i].shift_status == 0) {
+    //       lista0.add(_alunosPresentes[i]);
+    //       print(lista0[i].name_aluno);
+    //     }
+    //     if (_alunosPresentes[i].shift_status == 1) {
+    //       lista1.add(_alunosPresentes[i]);
+    //     }
+    //     if (_alunosPresentes[i].shift_status == 2) {
+    //       lista2.add(_alunosPresentes[i]);
+    //     }
+    //     if (_alunosPresentes[i].shift_status == 3) {
+    //       lista3.add(_alunosPresentes[i]);
+    //     }
+    //     i++;
+    //   }
+    //   _alunosPresentes1 = lista0 + lista1 + lista2 + lista3;
+    // }
+
     Future mensagem(BuildContext context, String texto) async {
       return showDialog(
         context: context,
@@ -85,27 +121,72 @@ class _AlunosPresentesState extends State<AlunosPresentes> {
             child: ListView.builder(
               itemCount: _alunosPresentes.length,
               itemBuilder: (_, indice) {
-                //ParentsModel _responsavel = _responsaveis[indice];
                 ShiftModel _aluno = _alunosPresentes[indice];
+
                 return Card(
                   child: ListTile(
                       leading:
-                          _aluno.shift_status == 0 || _aluno.shift_status == 3
+                          _aluno.shift_status == 0 
                               ? IconButton(
+                                  onPressed: () {
+                                    setState(
+                                      () {
+                                        _aluno.shift_status = 3;
+                                        updateStatus(_aluno);
+                                        // ordenar();
+                                      },
+                                    );
+                                  },
                                   icon: Icon(Icons.cancel_outlined,
                                       color: Colors.red))
-                              : Container(),
-                      trailing:
-                          _aluno.shift_status == 0 && _aluno.shift_status != 3
+                              : SizedBox(height: 5, width: 5,),
+                      trailing: _aluno.shift_status == 0 &&
+                              _aluno.shift_status != 3
+                          ? IconButton(
+                              onPressed: () {
+                                setState(
+                                  () {
+                                    _aluno.shift_status = 1;
+                                    updateStatus(_aluno);
+                                    // ordenar();
+                                  },
+                                );
+                              },
+                              icon: Icon(
+                                Icons.car_repair,
+                                color: Colors.black,
+                                size: 30,
+                              ),
+                            )
+                          : _aluno.shift_status != 2 && _aluno.shift_status != 3
                               ? IconButton(
-                                  icon: Icon(
-                                  Icons.car_repair,
-                                  color: Colors.black,
-                                  size: 30,
-                                ))
-                              : IconButton(
+                                  onPressed: () {
+                                    setState(
+                                      () {
+                                        _aluno.shift_status = 2;
+                                        updateStatus(_aluno);
+                                        // ordenar();
+                                      },
+                                    );
+                                  },
                                   icon: Icon(Icons.pin_drop_outlined,
-                                      color: Colors.black)),
+                                      color: Colors.black),
+                                )
+                              : _aluno.shift_status == 3 ? Text(
+                                  "Faltou",
+                                  style: TextStyle(
+                                    color: Colors.red,
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 20,
+                                  ),
+                                ) : Text(
+                                  "Entregue",
+                                  style: TextStyle(
+                                    color: Colors.black,
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 20,
+                                  ),
+                                ) ,
                       title: Column(
                         crossAxisAlignment: CrossAxisAlignment.start,
                         children: [
