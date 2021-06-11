18c13fe20bccdb6a1a5440c3302368e672728228
Will <willianliduenha@hotmail.com>
2021-06-11 15:37:07 -0300

    ajuste na tela de ausencia, agora o botão salvar e o aviso estão na parte inferior da tela

--- lib/views/ausencia_aluno.dart
+++ lib/views/ausencia_aluno.dart
@@ -32,7 +32,8 @@ class _AusenciaAlunoState extends State<AusenciaAluno> {
 
     if (resposta != null) {
       await mensagem(context, "Ausência cadastrada com sucesso!");
-      Navigator.of(context).pushNamed('/paginainicialpais', arguments: _responsaveis.user.login);
+      Navigator.of(context)
+          .pushNamed('/paginainicialpais', arguments: _responsaveis.user.login);
     } else
       mensagem(context, "Ausência NÃO CADASTRADA!");
   }
@@ -177,165 +178,186 @@ class _AusenciaAlunoState extends State<AusenciaAluno> {
         ),
         centerTitle: false,
       ),
-      body: Container(
-        padding: EdgeInsets.symmetric(horizontal: 10),
-        width: double.infinity,
-        child: Form(
-          key: _formKey,
-          child: SingleChildScrollView(
-                      child: Column(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Container(
+            padding: EdgeInsets.symmetric(horizontal: 10),
+            width: double.infinity,
+            child: Column(
               children: [
-                SizedBox(
-                  height: 20,
-                ),
-                ausente.date == null
-                    ? Align(
-                        alignment: Alignment.topLeft,
-                        child: Container(
-                          decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(5),
-                              color: Colors.yellow),
-                          width: 250,
-                          height: 40,
-                          child: TextButton(
-                            onPressed: () {
-                              getDate();
-                            },
-                            child: Row(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: [
-                                Text(
-                                  "Selecionar data",
-                                  style: TextStyle(
-                                      fontSize: 20, fontWeight: FontWeight.bold),
+                Form(
+                  key: _formKey,
+                  child: SingleChildScrollView(
+                    child: Column(
+                      children: [
+                        SizedBox(
+                          height: 20,
+                        ),
+                        ausente.date == null
+                            ? Align(
+                                alignment: Alignment.topLeft,
+                                child: Container(
+                                  decoration: BoxDecoration(
+                                      borderRadius: BorderRadius.circular(5),
+                                      color: Colors.yellow),
+                                  width: 250,
+                                  height: 40,
+                                  child: TextButton(
+                                    onPressed: () {
+                                      getDate();
+                                    },
+                                    child: Row(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.center,
+                                      children: [
+                                        Text(
+                                          "Selecionar data",
+                                          style: TextStyle(
+                                              fontSize: 20,
+                                              fontWeight: FontWeight.bold),
+                                        ),
+                                        SizedBox(
+                                          width: 20,
+                                        ),
+                                        Icon(Icons.calendar_today,
+                                            color: Colors.black),
+                                      ],
+                                    ),
+                                    style: ButtonStyle(
+                                      foregroundColor:
+                                          MaterialStateProperty.all<Color>(
+                                              Colors.black),
+                                    ),
+                                  ),
                                 ),
-                                SizedBox(
-                                  width: 20,
+                              )
+                            : Container(
+                                child: Row(
+                                  children: [
+                                    Text(
+                                      "Data: ",
+                                      style: TextStyle(
+                                        fontSize: 20,
+                                        color: Colors.black,
+                                      ),
+                                    ),
+                                    SizedBox(
+                                      width: 10,
+                                    ),
+                                    Text(
+                                      ausente.date.day.toString() +
+                                          "/" +
+                                          ausente.date.month.toString() +
+                                          "/" +
+                                          ausente.date.year.toString(),
+                                      style: TextStyle(
+                                        fontSize: 20,
+                                        color: Colors.black,
+                                      ),
+                                    ),
+                                    SizedBox(
+                                      width: 15,
+                                    ),
+                                    IconButton(
+                                      icon: Icon(
+                                        Icons.calendar_today,
+                                        color: Colors.yellow,
+                                        size: 20,
+                                      ),
+                                      onPressed: () {
+                                        getDate();
+                                      },
+                                    )
+                                  ],
                                 ),
-                                Icon(Icons.calendar_today, color: Colors.black),
-                              ],
-                            ),
-                            style: ButtonStyle(
-                              foregroundColor:
-                                  MaterialStateProperty.all<Color>(Colors.black),
+                              ),
+                        SizedBox(
+                          height: 30,
+                        ),
+                        Align(
+                          alignment: Alignment.topLeft,
+                          child: Text(
+                            "Trajeto / Percurso",
+                            style: TextStyle(
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold,
                             ),
                           ),
                         ),
-                      )
-                    : Container(
-                        child: Row(
-                          children: [
-                            Text(
-                              "Data: ",
-                              style: TextStyle(
-                                fontSize: 20,
-                                color: Colors.black,
-                              ),
-                            ),
-                            SizedBox(
-                              width: 10,
-                            ),
-                            Text(
-                              ausente.date.day.toString() +
-                                  "/" +
-                                  ausente.date.month.toString() +
-                                  "/" +
-                                  ausente.date.year.toString(),
-                              style: TextStyle(
-                                fontSize: 20,
-                                color: Colors.black,
-                              ),
-                            ),
-                            SizedBox(
-                              width: 15,
-                            ),
-                            IconButton(
-                              icon: Icon(
-                                Icons.calendar_today,
-                                color: Colors.yellow,
-                                size: 20,
-                              ),
-                              onPressed: () {
-                                getDate();
-                              },
-                            )
-                          ],
+                        SizedBox(
+                          height: 20,
                         ),
-                      ),
-                SizedBox(
-                  height: 30,
-                ),
-                Align(
-                  alignment: Alignment.topLeft,
-                  child: Text(
-                    "Trajeto / Percurso",
-                    style: TextStyle(
-                      fontSize: 20,
-                      fontWeight: FontWeight.bold,
+                        listviewIdaVolta(listTrajeto),
+                        SizedBox(
+                          height: 35,
+                        ),
+                      ],
                     ),
                   ),
                 ),
-                SizedBox(
-                  height: 20,
-                ),
-                listviewIdaVolta(listTrajeto),
-                SizedBox(
-                  height: 35,
-                ),
-                Container(
-                  width: double.infinity,
-                  height: 45,
-                  child: TextButton(
-                    onPressed: () async {
-                      if (ausente.date != null) {
-                        if (ausente.turno_ida != null &&
-                            ausente.turno_volta != null) {
-                          bool respostaMSG = await mensagemVerificacao(context);
-                          print(respostaMSG);
-                          if (respostaMSG) {
-                            await cadastrarAusente(context);
-                          }
-                        } else {
-                          await mensagem(
-                              context, "Você deve selecionar o tipo de trajeto");
-                        }
-                      } else {
-                        await mensagem(context, "Você deve selecionar uma data");
-                      }
-                    },
-                    child: Text(
-                      "Salvar",
-                      style: TextStyle(
-                        fontSize: 22,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                    style: ButtonStyle(
-                      foregroundColor:
-                          MaterialStateProperty.all<Color>(Colors.black),
-                      backgroundColor:
-                          MaterialStateProperty.all<Color>(Colors.yellow),
-                    ),
-                  ),
-                ),
-                SizedBox(
-                  height: 20,
-                ),
-                Row(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    Icon( Icons.warning_outlined, color: Colors.red,),
-                    SizedBox(width: 5,),
-                    Text("ATENÇÃO, APÓS SALVAR NÃO SERÁ POSSÍVEL DESFAZER A AÇÃO!",
-                        style: TextStyle(
-                          decoration: TextDecoration.underline,
-                        )),
-                  ],
-                )
               ],
             ),
           ),
-        ),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Icon(
+                    Icons.warning_outlined,
+                    color: Colors.red,
+                  ),
+                  SizedBox(
+                    width: 5,
+                  ),
+                  Text(
+                      "ATENÇÃO, APÓS SALVAR NÃO SERÁ POSSÍVEL DESFAZER A AÇÃO!",
+                      style: TextStyle(
+                        decoration: TextDecoration.underline,
+                      )),
+                ],
+              ),
+              Container(
+                width: double.infinity,
+                height: 45,
+                child: TextButton(
+                  onPressed: () async {
+                    if (ausente.date != null) {
+                      if (ausente.turno_ida != null &&
+                          ausente.turno_volta != null) {
+                        bool respostaMSG = await mensagemVerificacao(context);
+                        print(respostaMSG);
+                        if (respostaMSG) {
+                          await cadastrarAusente(context);
+                        }
+                      } else {
+                        await mensagem(
+                            context, "Você deve selecionar o tipo de trajeto");
+                      }
+                    } else {
+                      await mensagem(context, "Você deve selecionar uma data");
+                    }
+                  },
+                  child: Text(
+                    "Salvar",
+                    style: TextStyle(
+                      fontSize: 22,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  style: ButtonStyle(
+                    foregroundColor:
+                        MaterialStateProperty.all<Color>(Colors.black),
+                    backgroundColor:
+                        MaterialStateProperty.all<Color>(Colors.yellow),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }
